4865853d42d7debc2b1b28c4f046101073484f3d
KW <kenneth_sl_wong@msn.com>
2022-07-20 18:29:31 -0400

    adjusted footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,9 @@ import React from "react";
 
 function Footer() {
     return(
-        <footer className="px-2 py-2 d-flex justify-content-center">
+        <footer className="px-2 py-4 d-flex justify-content-center">
             <a href="https://github.com/KennWg" className="px-3" target="_blank" rel="noreferrer noopener">GitHub</a>
             <a href="https://www.linkedin.com/in/kenneth-wong-752829227/"className="px-3" target="_blank" rel="noreferrer noopener">LinkedIn</a>
-            <a href="https://stackoverflow.com/users/17565463/kennwg"className="px-3" target="_blank" rel="noreferrer noopener">Stack Overflow</a>
         </footer>
     );
 };
