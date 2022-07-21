1a5aedea10f30ed1260cf9ce9f3bf6a4ed337a18
KW <kenneth_sl_wong@msn.com>
2022-07-21 19:41:31 -0400

    added form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,3 +1,4 @@
+import emailjs from '@emailjs/browser';
 import React, { useState, useRef } from 'react';
 import { validateEmail } from '../../utils/helpers';
 
@@ -10,12 +11,12 @@ function ContactForm(props) {
     const nameEl = useRef(0);
     const emailEl = useRef(0);
     const messageEl = useRef(0);
+    const formEl = useRef(0);
 
 
     function handleChange(e) {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
-            console.log(isValid);
             if (!isValid) {
                 setErrorMessage('Your email is invalid.');
             } else {
@@ -35,6 +36,11 @@ function ContactForm(props) {
 
     function handleSubmit(e) {
         e.preventDefault();
+
+        if(formState.name && formState.email && formState.message){
+            emailjs.sendForm('service_5f1588i', 'template_q1lrbff', formEl.current, process.env.REACT_APP_API_KEY);
+        }
+
         nameEl.current.value = '';
         emailEl.current.value = '';
         messageEl.current.value = '';
@@ -44,7 +50,7 @@ function ContactForm(props) {
     return (
         <section className='px-2 py-2' ref={contactRef}>
             <h2 className='mx-5 my-5 text-center'>Contact me</h2>
-            <form id="contact-form ml-5" onSubmit={handleSubmit}>
+            <form id="contact-form ml-5" onSubmit={handleSubmit} ref={formEl}>
                 <div className='py-2 px-2 d-flex row'>
                     <label htmlFor="name" className='px-2 col-3'>Name:</label>
                     <input type="text" className="col-9" defaultValue={name} onBlur={handleChange} name="name" ref={nameEl}/>
